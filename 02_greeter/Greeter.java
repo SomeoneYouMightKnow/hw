da55867f6fd1fca5308e07deb3112e3a6b7231d0
Thomas Wong <twong3.1415926@gmail.com>
2014-09-15 09:27:28 -0400

    Edited Greeter.java

--- 02_greeter/Greeter.java
+++ 02_greeter/Greeter.java
@@ -7,6 +7,8 @@ public class Greeter {
 	public void greet() {
 		System.out.println("Hello world!");
         }
+    //If I used private instead of public, it would be unusable in Driver.java
+    //Unless ungreet was in another (public) in-class method such as greet()
 	public void ungreet() {
 	    System.out.println("I'm out");
 	}
