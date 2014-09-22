aacd0ff750aaf7b7a46df4534b0f2d8b60c5616f
Thomas Wong <twong3.1415926@gmail.com>
2014-09-22 01:07:14 -0400

    Created Driver.java

---
+++ 03_strings/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args){
+        String s="thomas wong";
+	var i = s.indexOf(" ");
+	var first = s.substring(0,i);
+	var last = s.substring(i+1);
+    }
+}
