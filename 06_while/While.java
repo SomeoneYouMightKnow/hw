7f079f7e7dec080edf53b42e76f5846a9b1e7986
Thomas Wong <twong3.1415926@gmail.com>
2014-10-01 00:18:21 -0400

    hw06

--- 06_while/While.java
+++ 06_while/While.java
@@ -47,7 +47,7 @@ public class While(){
 	}
 	return output;
     }
-
+    //Does not work for "yak123ya." Returns "Exception:java.lang.StringIndexOutOfBoundsException: String index out of range: 9 (line number:5)"
 
 
     //stringMatch Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings. 
