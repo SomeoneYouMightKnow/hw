cfc54fe3b0d95d97b4a00e5bc6a9c473b550ceba
Thomas Wong <twong3.1415926@gmail.com>
2014-10-02 02:45:43 -0400

    hw07

--- 07_Shapes/Driver.java
+++ 07_Shapes/Driver.java
@@ -3,7 +3,10 @@ public class Driver {
 	Shapes s = new Shapes();
 	System.out.print(s.box(4,5));
 	System.out.print(s.box(5,4));
-
+	System.out.print(s.tri1(5));
+	System.out.print(s.tri1(7));
+	System.out.print(s.tri2(5));
+	System.out.print(s.tri2(7));
     }
 
 }
--- 07_Shapes/Shapes.java
+++ 07_Shapes/Shapes.java
@@ -16,11 +16,41 @@ public class Shapes {
     }
 
     public String tri1(int a) {
-	int rowcounter = 0;
+	int rowcounter = 1;
+	int starcounter = 1;
 	String output = "";
-	while (rowcounter + 1 < a) {
-	    
+	while (rowcounter < a) {
+	    while (starcounter > 0) {
+		output = output + "*";
+		starcounter = starcounter - 1;
+	    }
+	    output = output + "\n";
+	    rowcounter = rowcounter + 1;
+	    starcounter = rowcounter;
 	}
+	return output;
+    }
+    
+    public String tri2(int a) {
+	int rowcounter = 1;
+	int starcounter = 1;
+	int spacecounter = a - 1;
+	String output = "";
+	while (rowcounter < a) {
+	    while (spacecounter > 0) {
+		output = output + " ";
+		spacecounter = spacecounter 1 1;
+	    }
+	    while (starcounter > 0) {
+		output = output + "*";
+		starcounter = starcounter - 1;
+	    }
+	    output = output + "\n";
+	    rowcounter = rowcounter + 1;
+	    starcounter = rowcounter;
+	    spacecounter = a - starcounter;
+	}
+	return output;
     }
 
 }
