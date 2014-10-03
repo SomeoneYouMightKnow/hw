ea7dec78e3e5869cbadfc7b52db6a006165a1d5b
Thomas Wong <twong3.1415926@gmail.com>
2014-10-03 01:05:59 -0400

    hw_07b

--- 07_Shapes/Driver.java
+++ 07_Shapes/Driver.java
@@ -2,11 +2,22 @@ public class Driver {
     public static void main(String[] args){
 	Shapes s = new Shapes();
 	System.out.print(s.box(4,5));
-	System.out.print(s.box(5,4));
+	System.out.print(s.box(5,6));
 	System.out.print(s.tri1(5));
+	System.out.print(s.tri1(6));
 	System.out.print(s.tri1(7));
 	System.out.print(s.tri2(5));
+	System.out.print(s.tri2(6));
 	System.out.print(s.tri2(7));
+	System.out.print(s.tri3(5));
+	System.out.print(s.tri3(6));
+	System.out.print(s.tri3(7));
+	System.out.print(s.diamond(5));
+	System.out.print(s.diamond(6));
+	System.out.print(s.diamond(7));
+	System.out.print(s.tri4(5));
+	System.out.print(s.tri4(6));
+	System.out.print(s.tri4(7));
     }
 
 }
--- 07_Shapes/Shapes.java
+++ 07_Shapes/Shapes.java
@@ -16,58 +16,107 @@ public class Shapes {
     }
 
     public String tri1(int a) {
-	int rowcounter = 1;
-	int starcounter = 1;
+	int rowcount = 1;
+	int starcount = 1;
 	String output = "";
-	while (rowcounter < a) {
-	    while (starcounter > 0) {
+	while (rowcount < a) {
+	    while (starcount > 0) {
 		output = output + "*";
-		starcounter = starcounter - 1;
+		starcount = starcount - 1;
 	    }
 	    output = output + "\n";
-	    rowcounter = rowcounter + 1;
-	    starcounter = rowcounter;
+	    rowcount = rowcount + 1;
+	    starcount = rowcount;
 	}
 	return output;
     }
     
     public String tri2(int a) {
-	int rowcounter = 1;
-	int starcounter = 1;
-	int spacecounter = a - 1;
+	int rowcount = 1;
+	int starcount = 1;
+	int spacecount = a - 1;
 	String output = "";
-	while (rowcounter < a) {
-	    while (spacecounter > 1) {
+	while (rowcount < a) {
+	    while (spacecount > 1) {
 		output = output + " ";
-		spacecounter = spacecounter - 1;
+		spacecount = spacecount - 1;
 	    }
-	    while (starcounter > 0) {
+	    while (starcount > 0) {
 		output = output + "*";
-		starcounter = starcounter - 1;
+		starcount = starcount - 1;
 	    }
 	    output = output + "\n";
-	    rowcounter = rowcounter + 1;
-	    starcounter = rowcounter;
-	    spacecounter = a - starcounter;
+	    rowcount = rowcount + 1;
+	    starcount = rowcount;
+	    spacecount = a - starcount;
 	}
 	return output;
     }
 
     public String tri3(int a) {
-	int rowcounter;
-	int starcounter;
-	int spacecounter;
+	int rowcount;
+	int starcount;
+	int spacecount;
 	String output="";
-	// For row n, there are 1 + 2(n-1) stars
-	// For row n, there are a - n
-	for(rowcounter=0;rowcounter=a;rowcounter--){
-	    for(spacecounter=a-1;spacecounter=0;spacecounter--){
+	for(rowcount=a;rowcount>0;rowcount--){
 
-	    }	
-	    for(starcounter=1;starcounter=1+2*(rowcounter
+	    // For row n, there are a - n spaces. n starts at 1, increases by 1
+	    for(spacecount=rowcount-1;spacecount>0;spacecount--){
+		output = output + " ";
+	    }
 
-
-	    
+	    // For row n, there are 1 + 2(n-1) stars
+	    for(starcount=1;starcount<=1+2*(a-rowcount);starcount++){
+		output = output + "*";
+	    }
+	    output = output + "\n";
 	}
+	return output;
+
+    }
+
+    public String diamond(int a){
+	int rowcount;
+	int spacecount;
+	int starcount;
+	String output = "";
+	for (rowcount=0;rowcount<=a;rowcount++){
+	    if (rowcount < a/2){
+        	for (spacecount=(a/2)-rowcount;spacecount>0;spacecount--){
+		    output = output + " ";
+		}
+		for (starcount=1;starcount<=1+2*(rowcount);starcount++){
+		    output = output + "*";
+		}
+	    }
+	    else{
+		for (spacecount=0;spacecount<Math.abs(a/2-rowcount);spacecount++){
+		    output = output + " ";
+		}
+		for (starcount=1+2*(a-rowcount-a%2);starcount>0;starcount--){
+		    output = output + "*";
+		}
+	    }
+	    output = output + "\n";
+	}
+	return output;
+    }
+
+    public String tri4(int a){
+	int rowcount;
+	int spacecount;
+	int starcount;
+	String output ="";
+	for (rowcount=0;rowcount<a;rowcount++){
+	    for (spacecount=rowcount;spacecount>0;spacecount--){
+		output = output + " ";
+	    }
+	    for (starcount=a-rowcount;starcount>0;starcount--){
+		output = output + "*";
+	    }     
+	    output = output + "\n";
+	}
+	return output;
+    }
 
 }
