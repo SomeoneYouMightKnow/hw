fc10caa66ea484393d788d43372cc82c059cc4d8
Thomas Wong <twong3.1415926@gmail.com>
2014-10-20 09:03:12 -0400

    Notes from October 20

--- Stuyablo/Basechar.java
+++ Stuyablo/Basechar.java
@@ -1,5 +1,8 @@
 //superclass for Warrior and Mage
-public class Basechar{
+//abstract class means you can't create a Basechar without making a subclass
+//i.e. Basechar b = new Basechar() won't work
+//inheritance still works
+public abstract class Basechar{
     private int health = 20;
     private String name;
 
@@ -11,9 +14,14 @@ public class Basechar{
 	setName(n);
     }
 
-    public void attack(Basechar other){
+    /*   public void attack(Basechar other){
 	System.out.println(this.getName()+" is attacking "+other.getName());
     }
+    */
+    //Mage and Warrior have their own attacks.
+    //abstract methods must be implemented in the subclass
+    public void attack(Basechar other);
+    //method body will be coded in subclass
 
     public void setName(String name){
 	this.name = name; //this.___ refers to instance var/methods of this class
