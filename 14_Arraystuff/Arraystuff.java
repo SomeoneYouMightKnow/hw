fc5ecc719e1e4412954d7962092a544e2be45859
Thomas Wong <twong3.1415926@gmail.com>
2014-10-28 00:55:20 -0400

    hw14

--- 14_Arraystuff/Arraystuff.java
+++ 14_Arraystuff/Arraystuff.java
@@ -39,9 +39,9 @@ public class Arraystuff {
     }
 
     public int maxVal(int[] a){
-	int max;
-	for ( int i = 0; i < a.length-1; i++){
-	    if (a[i] > a[i+1]){
+	int max = 0;
+	for ( int i = 0; i < a.length; i++){
+	    if (a[i] > m){
 		max = a[i];
 	    }
 	}
