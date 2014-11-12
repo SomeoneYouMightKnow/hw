db4685eb59fdc7da10e0559cf28186464dc99f3f
Thomas Wong <twong3.1415926@gmail.com>
2014-11-12 09:27:38 -0500

    ArrayList

---
+++ 15_Sarray/Driver.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	ArrayList<String> al;
+	al = new ArrayList<String>();
+
+	System.out.println(al);
+
+	al.add("hello");
+	System.out.println(al);
+
+	al.add("world");
+	System.out.println(al);
+
+		
+	al.add("33.2");
+	System.out.println(al);
+
+
+
+	Random rnd = new Random();
+	ArrayList<Integer> a1 = new Arraylist<Integer>();
+	for (int i = 0; i < 20; i++) {
+	    a1.add(r.nextInt(5));
+	}
+    	System.out.println(ai);
+
+
+
+    }
