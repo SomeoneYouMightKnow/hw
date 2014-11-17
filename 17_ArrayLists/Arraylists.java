e1ebb382ebdd729b3d82c0b3ab6dfa4fed2d70ab
Thomas Wong <twong3.1415926@gmail.com>
2014-11-17 09:24:45 -0500

    Arraylists

--- 17_ArrayLists/Arraylists.java
+++ 17_ArrayLists/Arraylists.java
@@ -11,24 +11,41 @@ public class Arraylists {
 	ai.add(i);
     }
 
-    public void swap(int indexA, int indexB){
-	int safe = ai.get(indexA);
-	ai.set(indexA, ai.get(indexB));
+    
+    public ArrayList<Integer> shuffle1() {
+	ArrayList<Integer> result = new ArrayList<Integer>();
+        while (ai.size()>0) {
+	    int i = rnd.nextInt(ai.size());
+	    int v = ai.remove(i);
+	    result.add(v); //hidden complexity. remove() and add() loop through the ArrayList, "behind the scenes"
+	}
+	return result; //Creates new ArrayList, uses more space, Might be undesirable
+    }
+
+    public ArrayList<Integer> shuffle2() {
+	ArrayList<Integer> result = new ArrayList<Integer>();
+	for (int s = ai.size(); s > 0; s--) {
+	    int i = rnd.nextInt(s);
+	    int v = ai.remove(i);
+	    ai.add(v);	//move the int at a random index to the end.
+	}
+	return result;
+	//Slower, saves space, loses original ArrayList
+    }
+
+    public ArrayList<Integer> shuffle3() {
+	ArrayList<Integer> result = new ArrayList<Integer>();
+	for (int s = ai.size(); s > 0; s--) {
+	    int i = rnd.nextInt(s);
+
+	    int temp = ai.get(i);
+	    ai.set( i , ai.get(s));
+	    ai.set( s , temp );
+
+
+	}
     }
     
-    //0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    public void scramble() {
-	int thingOne;
-	int thingTwo;
-        int i = rnd.nextInt(ai.size()) ; //
-	while (????){
-	    //????????????????????????
-       
-	}
-
-	    i = rnd.nextInt(ai.size());
-    }
-
 
     public static void main( String[] args ) {
 	System.out.println(ai);
