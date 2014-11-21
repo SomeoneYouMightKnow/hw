fb7cffb32868db7750e6139ff380e4c2c3eef815
Thomas Wong <twong3.1415926@gmail.com>
2014-11-21 09:07:14 -0500

    Readfile.java added

---
+++ 18_2D/Readfile.java
@@ -0,0 +1,24 @@
+import java.io.*; //Don't import the .*
+
+public class Readfile {
+
+    public static void main(String[] args) {
+	Scanner sc = null;
+	
+	try{
+	    sc = new Scanner(new File("words.txt"));
+	} catch (Exception e) {
+	    System.out.println("File not found");
+	    System.exit(0);
+	}
+
+	//Don't have the whole program in the try..catch
+	//Make sure sc exists and is initialized outside of the try..catch
+	while (sc.hasNext()) {
+	    String s = sc.next();
+	    System.out.println("Next line: "+s);
+	}
+
+    }
+    
+}
