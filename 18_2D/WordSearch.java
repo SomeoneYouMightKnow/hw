58dc1f7515991f88c6166bb8d4a53249da828168
Thomas Wong <twong3.1415926@gmail.com>
2014-12-02 01:30:33 -0500

    WordSearch

--- 18_2D/WordSearch.java
+++ 18_2D/WordSearch.java
@@ -52,27 +52,62 @@ public class WordSearch {
 	return s;
     }
 	
+    /*
+      public void addWordH( String w, int row, int col) {
+      boolean illegalOverlap = false;
+      for (int i = 0; i < w.length() - 1; i++) {
+      if ((board[row][col+i] != '.') && (board[row][col+i] != w.charAt(i))) {
+      illegalOverlap = true;
+      break;
+      }
+      }
 
-    public void addWordH( String w, int row, int col) {
+      if (!illegalOverlap){
+      for (int j = 0; j < w.length() - l; j++) {
+      board[row][j+col] = w.charAt(j);
+      }
+      }
+
+      /*
+      for (int i = 0; i < w.length(); i++) {
+      board[row][c] = w.charAt(i); //replaces any letters already there
+      c++; //could go out of bounds
+      }
+    
+      }
+    */
+
+    public boolean validInsertion( String w, int row, int col, int deltaRow, int deltaCol) {
 	boolean illegalOverlap = false;
 	for (int i = 0; i < w.length() - 1; i++) {
 	    if ((board[row][col+i] != '.') && (board[row][col+i] != w.charAt(i))) {
 		illegalOverlap = true;
+		break;
 	    }
 	}
 
-	if (!illegalOverlap){
-	    for (int j = 0; j < w.length() - l; j++) {
-		board[row][j+col] = w.charAt(j);
-	    }
-	}
+	boolean outOfBounds;
+	deltaRow = deltaRow * w.length();
+	deltaCol = deltaCol * w.length();
 
-	/*
-	  for (int i = 0; i < w.length(); i++) {
-	  board[row][c] = w.charAt(i); //replaces any letters already there
-	  c++; //could go out of bounds
-	  }
-	*/
+	if ((deltaRow == 0) && (deltaCol == 0)) {
+	    outOfBounds = false;
+	}
+	if ( row + deltaRow <= 0 ||
+	     row + deltaRow >= board.length ||
+	     col + deltaCol <= 0 ||
+	     col + deltaCol >= board[0].length
+	     )
+	    {
+		outOfBounds = true;
+	    }
+	return !(illegalOverlap || outOfBounds);
+    }
+
+    public boolean addWord(String w) {
+	
+
+
     }
 
     public void makeKey() {
@@ -102,6 +137,7 @@ public class WordSearch {
 	for (i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[0].length; j++) {
 		if (board[i][j] == '.') {
+		    String letters = "abcdefghijklmnopqrstuvwxyz";
 		    // board[i][j] = (char)((int)'a'+r.nextInt(26));
 		    board[i][j] = letters.charAt(r.nextInt(letters.length()));
 		}
